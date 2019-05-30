5326d880978bb58f69e6b5edda441a395b781135
Sam Markham <sam.e.markham@gmail.com>
2019-05-30 00:30:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,59 +5,59 @@ require 'pry'
 # - no more names, just number 
 # - the line is currently [254, 255] then next one 256
 
-$array = [254, 255]
+# $array = [254, 255]
 
-def line
-  if $array.empty?
-    puts "The line is currently empty."
-  else 
-    customer_line = $array.map.each_with_index { |x, i| "#{i + 1}. #{x}" }
-    puts "The line is currently: " << customer_line.join(" ")
-  end 
-end 
-
-def take_a_number
-  string = $array.last + 1 
-  $array.push(string)
-  number_in_line = $array.index(string) + 1 
-  puts "Welcome, #{string}. You are number #{number_in_line} in line."
-end 
-  
-def now_serving
-  if $array.empty?
-    puts "There is nobody waiting to be served!"
-  else 
-    customer = $array[0]
-    puts "Currently serving #{customer}."
-    $array.shift 
-  end 
-end 
-  
-
-# def line(array)
-
-#   if array.empty?
+# def line
+#   if $array.empty?
 #     puts "The line is currently empty."
 #   else 
-#     customer_line = array.map.each_with_index { |x, i| "#{i + 1}. #{x}" }
+#     customer_line = $array.map.each_with_index { |x, i| "#{i + 1}. #{x}" }
 #     puts "The line is currently: " << customer_line.join(" ")
 #   end 
 # end 
 
-# def take_a_number(array, string)
-#   array.push(string)
-#   number_in_line = array.index(string) + 1 
+# def take_a_number
+#   string = $array.last + 1 
+#   $array.push(string)
+#   number_in_line = $array.index(string) + 1 
 #   puts "Welcome, #{string}. You are number #{number_in_line} in line."
 # end 
   
-# def now_serving(array)
-#   if array.empty?
+# def now_serving
+#   if $array.empty?
 #     puts "There is nobody waiting to be served!"
 #   else 
-#     customer = array[0]
+#     customer = $array[0]
 #     puts "Currently serving #{customer}."
-#     array.shift 
+#     $array.shift 
 #   end 
 # end 
+  
+
+def line(array)
+
+  if array.empty?
+    puts "The line is currently empty."
+  else 
+    customer_line = array.map.each_with_index { |x, i| "#{i + 1}. #{x}" }
+    puts "The line is currently: " << customer_line.join(" ")
+  end 
+end 
+
+def take_a_number(array, string)
+  array.push(string)
+  number_in_line = array.index(string) + 1 
+  puts "Welcome, #{string}. You are number #{number_in_line} in line."
+end 
+  
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else 
+    customer = array[0]
+    puts "Currently serving #{customer}."
+    array.shift 
+  end 
+end 
   
 
